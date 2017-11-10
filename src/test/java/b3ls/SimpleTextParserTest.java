f11c2aebdf59f80456350fccbeda1184a7eaa389
Ben Craig <ben.craig@sita.aero>
2017-11-10 17:47:30 +0000

    Add in missing @Test annotation.

--- src/test/java/b3ls/SimpleTextParserTest.java
+++ src/test/java/b3ls/SimpleTextParserTest.java
@@ -5,154 +5,150 @@ import org.junit.Test;
 
 public class SimpleTextParserTest {
 
-    @Test
-    public void exampleOne_flightInformation() throws InvalidRawStringException {
-        // Assume the string is "AC1213FRAYUL"
-        // This can be defined as:
-        //      - Carrier       [2 characters]
-        //      - Flight Number [4 characters]
-        //      - Departure     [3 characters]
-        //      - Arrival       [3 characters]
+  @Test
+  public void exampleOne_flightInformation() throws InvalidRawStringException {
+    // Assume the string is "AC1213FRAYUL"
+    // This can be defined as:
+    //      - Carrier       [2 characters]
+    //      - Flight Number [4 characters]
+    //      - Departure     [3 characters]
+    //      - Arrival       [3 characters]
 
-        // Note; typically, setup like this would be hidden in a factory class.
-        RawGroup rawGroup = new RawGroup("FlightInfo");
+    // Note; typically, setup like this would be hidden in a factory class.
+    RawGroup rawGroup = new RawGroup("FlightInfo");
 
-        rawGroup.addRawFieldParser(new ParseableRawField(new RawField(2, "Carrier")));
-        rawGroup.addRawFieldParser(new ParseableRawField(new RawField(4, "FlightNumber")));
-        rawGroup.addRawFieldParser(new ParseableRawField(new RawField(3, "Departure")));
-        rawGroup.addRawFieldParser(new ParseableRawField(new RawField(3, "Arrival")));
+    rawGroup.addRawFieldParser(new ParseableRawField(new RawField(2, "Carrier")));
+    rawGroup.addRawFieldParser(new ParseableRawField(new RawField(4, "FlightNumber")));
+    rawGroup.addRawFieldParser(new ParseableRawField(new RawField(3, "Departure")));
+    rawGroup.addRawFieldParser(new ParseableRawField(new RawField(3, "Arrival")));
 
-        SimpleTextParser parser = new SimpleTextParser();
-        parser.addRawGroupParser(new ParseableRawGroup(rawGroup));
+    SimpleTextParser parser = new SimpleTextParser();
+    parser.addRawGroupParser(new ParseableRawGroup(rawGroup));
 
-        String result = parser.parse("AC1213FRAYUL");
-        Assert.assertEquals("", result);
+    String result = parser.parse("AC1213FRAYUL");
+    Assert.assertEquals("", result);
 
-        String carrier = parser.getItem("FlightInfo", "Carrier");
-        Assert.assertEquals("AC", carrier);
+    String carrier = parser.getItem("FlightInfo", "Carrier");
+    Assert.assertEquals("AC", carrier);
 
-        String flightNumber = parser.getItem("FlightInfo", "FlightNumber");
-        Assert.assertEquals("1213", flightNumber);
+    String flightNumber = parser.getItem("FlightInfo", "FlightNumber");
+    Assert.assertEquals("1213", flightNumber);
 
-        String departure = parser.getItem("FlightInfo", "Departure");
-        Assert.assertEquals("FRA", departure);
+    String departure = parser.getItem("FlightInfo", "Departure");
+    Assert.assertEquals("FRA", departure);
 
-        String arrival = parser.getItem("FlightInfo", "Arrival");
-        Assert.assertEquals("YUL", arrival);
-    }
+    String arrival = parser.getItem("FlightInfo", "Arrival");
+    Assert.assertEquals("YUL", arrival);
+  }
 
-    @Test
-    public void exampleTwo_addressInformation() throws InvalidRawStringException {
-        // Assume the string is "CRAIG BEN   086-1234567 Made-Up Road   Somewhere   Austria   086-7654321 A.N.Other Rd.  Nowhere     Austria   "
-        // This can be defined as:
-        //      - Name
-        //          - Surname       [6 chars]
-        //          - First name    [6 chars]
-        //      - Home Address
-        //          - Phone number  [12 chars]
-        //          - Street        [15 chars]
-        //          - Town          [12 chars]
-        //          - Country       [10 chars]
-        //      - Work Address
-        //          - Phone number  [12 chars]
-        //          - Street        [15 chars]
-        //          - Town          [12 chars]
-        //          - Country       [10 chars]
+  @Test
+  public void exampleTwo_addressInformation() throws InvalidRawStringException {
+    // Assume the string is "CRAIG BEN   086-1234567 Made-Up Road   Somewhere   Austria   086-7654321 A.N.Other Rd.  Nowhere     Austria   "
+    // This can be defined as:
+    //      - Name
+    //          - Surname       [6 chars]
+    //          - First name    [6 chars]
+    //      - Home Address
+    //          - Phone number  [12 chars]
+    //          - Street        [15 chars]
+    //          - Town          [12 chars]
+    //          - Country       [10 chars]
+    //      - Work Address
+    //          - Phone number  [12 chars]
+    //          - Street        [15 chars]
+    //          - Town          [12 chars]
+    //          - Country       [10 chars]
 
-        RawGroup nameGroup = new RawGroup("Name");
-        RawGroup homeAddress = new RawGroup("HomeAddress");
-        RawGroup workAddress = new RawGroup("WorkAddress");
+    RawGroup nameGroup = new RawGroup("Name");
+    RawGroup homeAddress = new RawGroup("HomeAddress");
+    RawGroup workAddress = new RawGroup("WorkAddress");
 
-        nameGroup.addRawFieldParser(new ParseableRawField(new RawField(6, "Surname")));
-        nameGroup.addRawFieldParser(new ParseableRawField(new RawField(6, "Given")));
+    nameGroup.addRawFieldParser(new ParseableRawField(new RawField(6, "Surname")));
+    nameGroup.addRawFieldParser(new ParseableRawField(new RawField(6, "Given")));
 
-        homeAddress.addRawFieldParser(new ParseableRawField(new RawField(12, "PhoneNumber")));
-        homeAddress.addRawFieldParser(new ParseableRawField(new RawField(15, "Street")));
-        homeAddress.addRawFieldParser(new ParseableRawField(new RawField(12, "Town")));
-        homeAddress.addRawFieldParser(new ParseableRawField(new RawField(10, "Country")));
+    homeAddress.addRawFieldParser(new ParseableRawField(new RawField(12, "PhoneNumber")));
+    homeAddress.addRawFieldParser(new ParseableRawField(new RawField(15, "Street")));
+    homeAddress.addRawFieldParser(new ParseableRawField(new RawField(12, "Town")));
+    homeAddress.addRawFieldParser(new ParseableRawField(new RawField(10, "Country")));
 
-        workAddress.addRawFieldParser(new ParseableRawField(new RawField(12, "PhoneNumber")));
-        workAddress.addRawFieldParser(new ParseableRawField(new RawField(15, "Street")));
-        workAddress.addRawFieldParser(new ParseableRawField(new RawField(12, "Town")));
-        workAddress.addRawFieldParser(new ParseableRawField(new RawField(10, "Country")));
+    workAddress.addRawFieldParser(new ParseableRawField(new RawField(12, "PhoneNumber")));
+    workAddress.addRawFieldParser(new ParseableRawField(new RawField(15, "Street")));
+    workAddress.addRawFieldParser(new ParseableRawField(new RawField(12, "Town")));
+    workAddress.addRawFieldParser(new ParseableRawField(new RawField(10, "Country")));
 
-        SimpleTextParser parser = new SimpleTextParser();
-        parser.addRawGroupParser(new ParseableRawGroup(nameGroup));
-        parser.addRawGroupParser(new ParseableRawGroup(homeAddress));
-        parser.addRawGroupParser(new ParseableRawGroup(workAddress));
+    SimpleTextParser parser = new SimpleTextParser();
+    parser.addRawGroupParser(new ParseableRawGroup(nameGroup));
+    parser.addRawGroupParser(new ParseableRawGroup(homeAddress));
+    parser.addRawGroupParser(new ParseableRawGroup(workAddress));
 
-        String result = parser.parse("CRAIG BEN   086-1234567 Made-Up Road   Somewhere   Austria   086-7654321 A.N.Other Rd.  Nowhere     Austria   ");
-        Assert.assertEquals("", result);
+    String result = parser.parse(
+        "CRAIG BEN   086-1234567 Made-Up Road   Somewhere   Austria   086-7654321 A.N.Other Rd.  Nowhere     Austria   ");
+    Assert.assertEquals("", result);
 
-        String surname = parser.getItem("Name", "Surname");
-        Assert.assertEquals("CRAIG ", surname);
+    String surname = parser.getItem("Name", "Surname");
+    Assert.assertEquals("CRAIG ", surname);
 
-        String given = parser.getItem("Name", "Given");
-        Assert.assertEquals("BEN   ", given);
+    String given = parser.getItem("Name", "Given");
+    Assert.assertEquals("BEN   ", given);
 
+    String homePhoneNumber = parser.getItem("HomeAddress", "PhoneNumber");
+    Assert.assertEquals("086-1234567 ", homePhoneNumber);
 
-        String homePhoneNumber = parser.getItem("HomeAddress", "PhoneNumber");
-        Assert.assertEquals("086-1234567 ", homePhoneNumber);
+    String homeStreet = parser.getItem("HomeAddress", "Street");
+    Assert.assertEquals("Made-Up Road   ", homeStreet);
 
-        String homeStreet = parser.getItem("HomeAddress", "Street");
-        Assert.assertEquals("Made-Up Road   ", homeStreet);
+    String homeTown = parser.getItem("HomeAddress", "Town");
+    Assert.assertEquals("Somewhere   ", homeTown);
 
-        String homeTown = parser.getItem("HomeAddress", "Town");
-        Assert.assertEquals("Somewhere   ", homeTown);
+    String homeCountry = parser.getItem("HomeAddress", "Country");
+    Assert.assertEquals("Austria   ", homeCountry);
 
-        String homeCountry = parser.getItem("HomeAddress", "Country");
-        Assert.assertEquals("Austria   ", homeCountry);
+    String workPhoneNumber = parser.getItem("WorkAddress", "PhoneNumber");
+    Assert.assertEquals("086-7654321 ", workPhoneNumber);
 
+    String workStreet = parser.getItem("WorkAddress", "Street");
+    Assert.assertEquals("A.N.Other Rd.  ", workStreet);
 
-        String workPhoneNumber = parser.getItem("WorkAddress", "PhoneNumber");
-        Assert.assertEquals("086-7654321 ", workPhoneNumber);
+    String workTown = parser.getItem("WorkAddress", "Town");
+    Assert.assertEquals("Nowhere     ", workTown);
 
-        String workStreet = parser.getItem("WorkAddress", "Street");
-        Assert.assertEquals("A.N.Other Rd.  ", workStreet);
+    String workCountry = parser.getItem("WorkAddress", "Country");
+    Assert.assertEquals("Austria   ", workCountry);
 
-        String workTown = parser.getItem("WorkAddress", "Town");
-        Assert.assertEquals("Nowhere     ", workTown);
+  }
 
-        String workCountry = parser.getItem("WorkAddress", "Country");
-        Assert.assertEquals("Austria   ", workCountry);
+  // #1
+  @Test
+  public void shouldHandleCallToGetNonExistingField() throws InvalidRawStringException {
+    RawGroup rawGroup = new RawGroup("FlightInfo");
+    rawGroup.addRawFieldParser(
+        new ParseableRawField(
+            new RawField(2, "AirlineDesignatorOfBoardingPassIssuer")));
 
-    }
+    SimpleTextParser parser = new SimpleTextParser();
+    parser.addRawGroupParser(new ParseableRawGroup(rawGroup));
 
-    // #1
-    @Test
-    public void shouldHandleCallToGetNonExistingField() throws InvalidRawStringException {
-        RawGroup rawGroup = new RawGroup("FlightInfo");
-        rawGroup.addRawFieldParser(
-                new ParseableRawField(
-                        new RawField(2, "AirlineDesignatorOfBoardingPassIssuer")));
+    String result = parser.parse("AC1213FRAYUL");
+    Assert.assertEquals("1213FRAYUL", result);
 
-        SimpleTextParser parser = new SimpleTextParser();
-        parser.addRawGroupParser(new ParseableRawGroup(rawGroup));
+    String resultString = parser.getItem("FlightInfo", "AirlineDesignatorOfBoardingPass");
 
-        String result = parser.parse("AC1213FRAYUL");
-        Assert.assertEquals("1213FRAYUL", result);
+    Assert.assertEquals("", resultString);
+  }
 
+  // #2
+  @Test(expected = InvalidRawStringException.class)
+  public void shouldNotAllowTextStringThatIsSmallerThanExpected() throws InvalidRawStringException {
+    RawGroup customer = new RawGroup("Customer");
+    RawField name = new RawField(10, "Given");
+    RawField surname = new RawField(10, "Surname");
 
-        String resultString = parser.getItem("FlightInfo", "AirlineDesignatorOfBoardingPass");
+    customer.addRawFieldParser(new ParseableRawField(name));
+    customer.addRawFieldParser(new ParseableRawField(surname));
 
-        Assert.assertEquals("", resultString);
-    }
+    SimpleTextParser parser = new SimpleTextParser();
+    parser.addRawGroupParser(new ParseableRawGroup(customer));
 
-    // #2
-    public void shouldNotAllowTextStringThatIsSmallerThanExpected() throws InvalidRawStringException {
-        RawGroup customer = new RawGroup("Customer");
-        RawField name = new RawField(10, "Given");
-        RawField surname = new RawField(10, "Surname");
-
-        customer.addRawFieldParser(new ParseableRawField(name));
-        customer.addRawFieldParser(new ParseableRawField(surname));
-
-        SimpleTextParser parser = new SimpleTextParser();
-        parser.addRawGroupParser(new ParseableRawGroup(customer));
-
-        String result = parser.parse("LILY BREID");
-
-
-    }
-
+    String result = parser.parse("LILY BREID");
+  }
 }
