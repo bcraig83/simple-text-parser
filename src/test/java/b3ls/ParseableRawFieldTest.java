a86e77bdefa01313c4290a0061f8e5c8e93dfe4d
Ben Craig <ben.craig@sita.aero>
2017-11-10 17:53:14 +0000

    Fix indenting

--- src/test/java/b3ls/ParseableRawFieldTest.java
+++ src/test/java/b3ls/ParseableRawFieldTest.java
@@ -5,16 +5,16 @@ import org.junit.Test;
 
 public class ParseableRawFieldTest {
 
-    @Test
-    public void shouldParseSimpleData() {
+  @Test
+  public void shouldParseSimpleData() {
 
-        RawField carrierCode = new RawField(2, "CarrierCode");
-        ParseableRawField fixture = new ParseableRawField(carrierCode);
+    RawField carrierCode = new RawField(2, "CarrierCode");
+    ParseableRawField fixture = new ParseableRawField(carrierCode);
 
-        String result = fixture.parse("AC1234");
+    String result = fixture.parse("AC1234");
 
-        Assert.assertEquals("1234", result);
-        Assert.assertEquals("AC", fixture.getContents());
-        Assert.assertEquals("CarrierCode", fixture.getName());
-    }
+    Assert.assertEquals("1234", result);
+    Assert.assertEquals("AC", fixture.getContents());
+    Assert.assertEquals("CarrierCode", fixture.getName());
+  }
 }
