970382d2d15a93d2a64f5564ef89f6ac098fa6fd
Ben Craig <ben.craig@sita.aero>
2017-11-10 17:53:29 +0000

    Add getErrorMessage to ParseableRawField

--- src/main/java/b3ls/ParseableRawField.java
+++ src/main/java/b3ls/ParseableRawField.java
@@ -2,31 +2,32 @@ package b3ls;
 
 public class ParseableRawField implements Parseable {
 
-    private RawField rawField;
+  private RawField rawField;
 
-    public ParseableRawField(RawField rawField) {
-        this.rawField = rawField;
-    }
+  public ParseableRawField(RawField rawField) {
+    this.rawField = rawField;
+  }
 
-    @Override
-    public String parse(String source) {
-        int amountToConsume = rawField.getSize();
-        rawField.setContents(source.substring(0, amountToConsume));
-        return source.substring(amountToConsume, source.length());
-    }
+  @Override
+  public String parse(String source) {
+    int amountToConsume = rawField.getSize();
+    rawField.setContents(source.substring(0, amountToConsume));
+    return source.substring(amountToConsume, source.length());
+  }
 
+  public String getName() {
+    return rawField.getName();
+  }
 
-    public String getName() {
-        return rawField.getName();
-    }
+  public String getContents() {
+    return rawField.getContents();
+  }
 
+  public int getSize() {
+    return rawField.getSize();
+  }
 
-    public String getContents() {
-        return rawField.getContents();
-    }
-
-    public int getSize() {
-        return rawField.getSize();
-    }
-
+  public String getErrorMessage() {
+    return rawField.getErrorMessage();
+  }
 }
