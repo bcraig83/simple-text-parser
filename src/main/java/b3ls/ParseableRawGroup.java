ec482540390e794e988a52114909fc63e85dac53
Ben Craig <ben.craig@sita.aero>
2017-11-10 18:01:40 +0000

    Add method to the decorator class.

--- src/main/java/b3ls/ParseableRawGroup.java
+++ src/main/java/b3ls/ParseableRawGroup.java
@@ -4,39 +4,43 @@ import java.util.Map;
 
 public class ParseableRawGroup implements Parseable {
 
-    private RawGroup rawGroup;
+  private RawGroup rawGroup;
 
-    ParseableRawGroup(RawGroup rawGroup) {
-        this.rawGroup = rawGroup;
+  ParseableRawGroup(RawGroup rawGroup) {
+    this.rawGroup = rawGroup;
+  }
+
+  @Override
+  public String parse(String source) {
+    Map<String, ParseableRawField> map = rawGroup.getMap();
+
+    String temp = source;
+    for (ParseableRawField group : map.values()) {
+      temp = group.parse(temp);
     }
 
-    @Override
-    public String parse(String source) {
-        Map<String, ParseableRawField> map = rawGroup.getMap();
+    return temp;
+  }
 
-        String temp = source;
-        for (ParseableRawField group : map.values()) {
-            temp = group.parse(temp);
-        }
+  String getValue(String key) throws RawFieldDoesNotExistException {
+    ParseableRawField rawField = rawGroup.getMap().get(key);
 
-        return temp;
+    if (rawField == null) {
+      throw new RawFieldDoesNotExistException();
     }
 
-    String getValue(String key) throws RawFieldDoesNotExistException {
-        ParseableRawField rawField = rawGroup.getMap().get(key);
+    return rawField.getContents();
+  }
 
-        if (rawField == null) {
-            throw new RawFieldDoesNotExistException();
-        }
+  String getName() {
+    return rawGroup.getName();
+  }
 
-        return rawField.getContents();
-    }
+  int getSize() {
+    return rawGroup.getSize();
+  }
 
-    String getName() {
-        return rawGroup.getName();
-    }
-
-    public int getSize() {
-        return rawGroup.getSize();
-    }
+  public Map<String, String> getErrors() {
+    return rawGroup.getErrors();
+  }
 }
